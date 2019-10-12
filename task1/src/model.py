6a555b0d483a5d17d7418b06a6b4adca596e4447
holbergchris <holbergchris@hotmail.com>
2019-10-12 17:52:03 +0200

    Added class for XGB estimator. Removed Param class and included tuning methods for the different estimators

--- task1/src/model.py
+++ task1/src/model.py
@@ -2,88 +2,26 @@
 import pandas as pd
 import numpy as np
 from lightgbm import LGBMRegressor
+from xgboost import XGBRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.linear_model import Lasso
-from sklearn.preprocessing import RobustScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from scipy.stats import randint as sp_randint
 from scipy.stats import uniform as sp_uniform
-from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin, clone
 
-class Param:
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
-
-    def optimizeLasso(self, scoring='r2', save=False):
-        X = self.X
-        y = self.y.y.values
-        lasso_ = Lasso(max_iter=5000)
-        par = {"alpha": [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]}
-        lasso_cv = GridSearchCV(lasso_, par, scoring=scoring, cv=5, verbose=1, n_jobs=-1)
-        lasso_cv.fit(X, y)
-        if save:
-            par_df = pd.DataFrame(lasso_cv.best_params_, index=[0]).to_csv("data/lasso_par.csv")
-        return lasso_cv.best_params_, lasso_cv.best_score_
-
-    def optimizeRF(self, scoring='r2', iter=100, save=False):
-        X = self.X
-        y = self.y.y.values
-        rf = RandomForestRegressor()
-        par = {'bootstrap': [True, False],
-               'max_depth': sp_randint(10, 80),
-               'max_features': ['auto', 'sqrt'],
-               'min_samples_leaf': [1, 2, 4],
-               'min_samples_split': [2, 5, 10],
-               'n_estimators': sp_randint(100, 800)}
-        rf_cv = RandomizedSearchCV(estimator=rf, param_distributions=par, n_iter=iter,
-                                   scoring=scoring, cv=5, verbose=1, n_jobs=-1)
-        rf_cv.fit(X, y)
-        if save:
-            pd.DataFrame(rf_cv.best_params_, index=[0]).to_csv("data/rf_par.csv")
-        return rf_cv.best_params_, rf_cv.best_score_
-
-    def optimizeLGB(self, score='r2', iter=100, save=False):
-        X_train, X_test, y_train, y_test  = train_test_split(self.X, self.y.y, test_size=.2)
-        fit_params = {"early_stopping_rounds": 30,
-                      "eval_metric": 'mse',
-                      "eval_set": [(X_test, y_test)],
-                      'eval_names': ['valid'],
-                      "verbose": 0}
-        param_test = {'num_leaves': sp_randint(6, 50),
-                      'min_child_samples': sp_randint(100, 500),
-                      'min_child_weight': [1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3],
-                      'subsample': sp_uniform(loc=0.2, scale=0.8),
-                      'colsample_bytree': sp_uniform(loc=0.4, scale=0.6),
-                      'reg_alpha': [0, 1e-1, 1, 2, 5, 7, 10, 50, 100],
-                      'reg_lambda': [0, 1e-1, 1, 5, 10, 20, 50, 100]}
-        lgbm = LGBMRegressor(max_depth=-1, silent=True, metric='None',
-                                 n_estimators=5000)
-        lgbm_cv = RandomizedSearchCV(estimator=lgbm, param_distributions=param_test, n_iter=iter,
-                                     scoring='r2', cv=5, verbose=1, n_jobs=-1)
-        lgbm_cv.fit(X_train, y_train, **fit_params)
-        if save:
-            pd.DataFrame(lgbm_cv.best_params_, index=[0]).to_csv("data/lgbm_par.csv")
-        return lgbm_cv.best_params_, lgbm_cv.best_score_
 
 class LGBM(BaseEstimator):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        self.model = LGBMRegressor(max_depth=-1, silent=True, n_estimators=5000, **self.__dict__)
+        self.model = LGBMRegressor(max_depth=-1, silent=True, **self.__dict__)
 
     def fit(self, X, y):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-        fit_params = {"early_stopping_rounds": 30,
-                  "eval_metric": 'mse',
-                  "eval_set": [(X_test, y_test)],
-                  'eval_names': ['valid'],
-                  "verbose": 0}
         params = dict()
-        self.model.fit(X_train, y_train, **fit_params)
+        self.model.fit(X, y, verbose=0)
         return self
 
     def predict(self, X):
@@ -96,6 +34,75 @@ class LGBM(BaseEstimator):
         for parameter, value in params.items():
             setattr(self, parameter, value)
         return self
+
+    def tune(self, X, y, score='r2', iter=100, save=False):
+        param_test = {'n_estimators': sp_randint(100, 1000),
+                      'num_leaves': sp_randint(6, 60),
+                      'min_child_samples': sp_randint(100, 500),
+                      'min_child_weight': [1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3],
+                      'subsample': sp_uniform(loc=0.2, scale=0.8),
+                      'colsample_bytree': sp_uniform(loc=0.4, scale=0.6),
+                      'reg_alpha': [0, 1e-1, 1, 2, 5, 7, 10, 50, 100],
+                      'reg_lambda': [0, 1e-1, 1, 5, 10, 20, 50, 100]}
+        lgbm = LGBMRegressor(max_depth=-1, silent=True, metric='None')
+        lgbm_cv = RandomizedSearchCV(estimator=lgbm, param_distributions=param_test, n_iter=iter,
+                                     scoring='r2', cv=5, verbose=1, n_jobs=-1)
+        lgbm_cv.fit(X, y, verbose=0)
+
+        if save:
+            pd.DataFrame(lgbm_cv.best_params_, index=[0]).to_csv("data/lgbm_par.csv")
+
+        for key in lgbm_cv.best_params_:
+            self.__dict__[key] = lgbm_cv.best_params_[key]
+
+        print("Best score:", lgbm_cv.best_score_, "obtained for\n", lgbm_cv.best_params_)
+
+
+class XGB(BaseEstimator):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        self.model = XGBRegressor(silent=True, **self.__dict__)
+
+    def fit(self, X, y):
+        params = dict()
+        self.model.fit(X, y, verbose=0)
+        return self
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def get_params(self, deep=True):
+        return {k: self.__dict__[k] for k in self.__dict__.keys() if k != 'model'}
+
+    def set_params(self, **params):
+        for parameter, value in params.items():
+            setattr(self, parameter, value)
+        return self
+
+    def tune(self, X, y, score='r2', iter=100, save=False):
+        param_test = {'n_estimators': sp_randint(100, 1000),
+                      'max_depth': sp_randint(4, 30),
+                      'num_leaves': sp_randint(6, 60),
+                      'min_child_samples': sp_randint(100, 500),
+                      'min_child_weight': [1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3],
+                      'subsample': sp_uniform(loc=0.2, scale=0.8),
+                      'colsample_bytree': sp_uniform(loc=0.4, scale=0.6),
+                      'reg_alpha': [0, 1e-1, 1, 2, 5, 7, 10, 50, 100],
+                      'reg_lambda': [0, 1e-1, 1, 5, 10, 20, 50, 100]}
+        xgb = XGBRegressor(silent=True, metric='None')
+        xgb_cv = RandomizedSearchCV(estimator=xgb, param_distributions=param_test, n_iter=iter,
+                                    scoring='r2', cv=5, verbose=1, n_jobs=-1)
+        xgb_cv.fit(X, y, verbose=0)
+
+        if save:
+            pd.DataFrame(xgb_cv.best_params_, index=[0]).to_csv("data/xgb_par.csv")
+
+        for key in xgb_cv.best_params_:
+            self.__dict__[key] = xgb_cv.best_params_[key]
+
+        print("Best score:", xgb_cv.best_score_, "obtained for\n", xgb_cv.best_params_)
+
+
 
 class RF(BaseEstimator):
     def __init__(self, **kwargs):
@@ -119,6 +126,27 @@ class RF(BaseEstimator):
             setattr(self, parameter, value)
         return self
 
+    def tune(self, X, y, scoring='r2', iter=100, save=False):
+        par = {'bootstrap': [True, False],
+               'max_depth': sp_randint(10, 80),
+               'max_features': ['auto', 'sqrt'],
+               'min_samples_leaf': [1, 2, 4],
+               'min_samples_split': [2, 5, 10],
+               'n_estimators': sp_randint(100, 800)}
+        rf = RandomForestRegressor()
+        rf_cv = RandomizedSearchCV(estimator=rf, param_distributions=par, n_iter=iter,
+                                   scoring=scoring, cv=5, verbose=1, n_jobs=-1)
+        rf_cv.fit(X, y)
+
+        if save:
+            pd.DataFrame(rf_cv.best_params_, index=[0]).to_csv("data/rf_par.csv")
+
+        for key in rf_cv.best_params_:
+            self.__dict__[key] = rf_cv.best_params_[key]
+
+        print("Best score:", rf_cv.best_score_, "obtained for\n", rf_cv.best_params_)
+
+
 class LassoRegressor(BaseEstimator):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -140,6 +168,43 @@ class LassoRegressor(BaseEstimator):
         for parameter, value in params.items():
             setattr(self, parameter, value)
         return self
+
+    def tune(self, X, y, scoring='r2', save=False):
+        par = {"alpha": [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]}
+        lasso = Lasso(max_iter=5000)
+        lasso_cv = GridSearchCV(lasso, par, scoring=scoring, cv=5, verbose=1, n_jobs=-1)
+        lasso_cv.fit(X, y)
+
+        if save:
+            par_df = pd.DataFrame(lasso_cv.best_params_, index=[0]).to_csv("data/lasso_par.csv")
+
+        for key in lasso_cv.best_params_:
+            self.__dict__[key] = lasso_cv.best_params_[key]
+
+        print("Best score:", lasso_cv.best_score_, "obtained for\n", lasso_cv.best_params_)
+
+
+class AveragingModels(BaseEstimator):
+    def __init__(self, models):
+        self.models = models
+
+    # we define clones of the original models to fit the data in
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+
+        return self
+
+    # Now we do the predictions for cloned models and average them
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
+
 
 def r2_cv(model, X, y):
     kf = KFold(5, shuffle=True).get_n_splits(X, y)
