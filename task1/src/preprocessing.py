5ba5f998ce993c0bde32e1206a998c21aa703004
holbergchris <holbergchris@hotmail.com>
2019-10-13 13:48:32 +0200

    Added more parameters to the outlier processing method. Added method for Box-Cox transforming response.

--- task1/src/preprocessing.py
+++ task1/src/preprocessing.py
@@ -3,11 +3,14 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import PowerTransformer
 
 class Preprocessor:
-  def __init__(self, X, X_test):
-    self.X = X
-    self.X_ = X_test
+  def __init__(self, X, X_test, y):
+    self.X = X.copy()
+    self.X_ = X_test.copy()
+    self.y = y.copy()
 
   def impute(self, fill="random"):
     for name in self.X.columns:
@@ -30,18 +33,31 @@ class Preprocessor:
       self.X_.loc[missing_, name] = imp_
     return self.X, self.X_
 
-  def processOutliers(self, method="if", remove=True):
+  def processOutliers(self, method="if", contamination=.1, remove=True):
     if method == "if":
-      clf = IsolationForest(n_estimators=300, max_samples=1000)
-      clf.fit(self.X)
+      train = self.X.merge(self.y, by='id')
+      clf = IsolationForest(n_estimators=100, max_samples=.6, max_features=.8,
+                            n_jobs=-1, bootstrap=True, contamination=contamination)
+      clf.fit(train)
       outliers = clf.predict(self.X) < 0
       if remove:
         self.X = self.X.iloc[~outliers, :]
+        self.y = self.y.iloc[~outliers, :]
+
+    return self.X, self.X_, self.y
+
+  def scaleData(self, scaler="normal"):
+    if scaler == "normal":
+      scaler = StandardScaler()
+    elif scaler == "robust":
+      scaler = RobustScaler()
+    self.X.iloc[:, :] = scaler.fit_transform(self.X)
+    self.X_.iloc[:, :] = scaler.transform(self.X_)
     return self.X, self.X_
 
-  def scaleData(self, center=True, normal=True):
-    if center & normal:
-      scaler = StandardScaler()
-      self.X.iloc[:,:] = scaler.fit_transform(self.X)
-      self.X_.iloc[:,:] = scaler.transform(self.X_)
-    return self.X, self.X_
+  def transformResponse(self, method='box-cox'):
+    if method == 'box-cox':
+      transformer = PowerTransformer()
+      self.y = transformer.fit_transform(self.y)
+    return self.y, transformer
+
