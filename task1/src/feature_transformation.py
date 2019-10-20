3cd64e371b16b2fc99c8461117ab29f250a4de38
holbergchris <holbergchris@hotmail.com>
2019-10-20 17:48:02 +0200

    Added method method for feature selection to the transformer class. Added class for stacked regression with meta learner.

--- task1/src/feature_transformation.py
+++ task1/src/feature_transformation.py
@@ -2,11 +2,15 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Lasso, Ridge, LinearRegression
+from sklearn.feature_selection import RFECV
 
 class Transformer:
-    def __init__(self, X, X_test):
+    def __init__(self, X, X_test, y):
         self.X = X.copy()
         self.X_ = X_test.copy()
+        self.y = y.copy()
 
     def reduceDimension(self, threshold=.9):
         pca = PCA()
@@ -17,3 +21,43 @@ class Transformer:
         self.X_ = self.X_.iloc[:, cum_variance < threshold]
         return self.X, self.X_
 
+    def selectFeatures(self, method="RFECV", save=False):
+        if method == "RFECV":
+            rf = RandomForestRegressor(n_jobs=-1, n_estimators=50, verbose=0)
+            rfecv = RFECV(estimator=rf, step=20, cv=5, scoring='r2', verbose=1)
+            rfecv.fit(self.X, self.y)
+            self.X = self.X.iloc[:, rfecv.support_]
+            self.X_ = self.X_.iloc[:, rfecv.support_]
+            if save:
+                pd.DataFrame({"features": rfecv.support_}).to_csv("data/best_features.csv")
+            return self.X, self.X_
+
+        if method == "SelectFrom":
+            ranks = {}
+            colnames = self.X.columns
+            lr = LinearRegression(normalize=True)
+            lr.fit(self.X, self.y)
+            ranks["LinReg"] = ranking(np.abs(lr.coef_), colnames)
+            ridge = Ridge(alpha=10)
+            ridge.fit(self.X, self.y)
+            ranks['Ridge'] = ranking(np.abs(ridge.coef_), colnames)
+            lasso = Lasso(alpha=.3)
+            lasso.fit(self.X, self.y)
+            ranks["Lasso"] = ranking(np.abs(lasso.coef_), colnames)
+            rf = RandomForestRegressor(n_jobs=-1, n_estimators=100, verbose=1)
+            rf.fit(self.X, self.y)
+            ranks["RF"] = ranking(rf.feature_importances_, colnames)
+
+            r = {}
+            for name in colnames:
+                r[name] = np.round(np.mean([ranks[method][name] for method in ranks.keys()]), 2)
+            mean_ranks = pd.DataFrame(list(r.items()), columns=['Feature', 'Mean Ranking'])
+            mean_ranks = mean_ranks.sort_values('Mean Ranking', ascending=False)
+            return mean_ranks.loc[:20, "Feature"]
+
+
+def ranking(ranks, names, order=1):
+    minmax = MinMaxScaler()
+    ranks = minmax.fit_transform(order*np.array([ranks]).T).T[0]
+    ranks = map(lambda x: round(x,2), ranks)
+    return dict(zip(names, ranks))
--- task1/src/model.py
+++ task1/src/model.py
@@ -220,7 +220,7 @@ class NN(BaseEstimator):
 
     def tune(self, X, y, scoring='r2', save=False):
         par = {
-            "input": [64, 128],
+            "input": [32, 64, 128],
             "hidden": [128, 256],
             "num_hidden": [2, 3, 4]
         }
@@ -256,6 +256,45 @@ class AveragingModels(BaseEstimator):
         ])
         return np.mean(predictions, axis=1)
 
+
+class StackingAveragedModels(BaseEstimator):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        X_ = X.reset_index()
+        y_ = y.reset_index()
+        X_.drop("id", axis=1, inplace=True)
+        y_.drop("id", axis=1, inplace=True)
+        self.base_models_ = [list() for k in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True)
+
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X_, y_):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X_.iloc[train_index, :], y_.iloc[train_index,:])
+                y_pred = instance.predict(X_.iloc[holdout_index, :])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+
+    # Do the predictions of all base models on the test data and use the averaged predictions as
+    # meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_])
+        return self.meta_model_.predict(meta_features)
 
 def r2_cv(model, X, y):
     kf = KFold(5, shuffle=True).get_n_splits(X, y)
